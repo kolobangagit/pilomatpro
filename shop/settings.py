0fed2ee4bbb333e07d61735be4796c1f53d1fdc3
berluskuni <berluskuni@gmail.com>
2023-12-28 16:35:49 +0200

    add form page

--- shop/settings.py
+++ shop/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'store.middlewares.AjaxMiddleware'
+    'store.middleware.AjaxMiddleware'
 ]
 
 ROOT_URLCONF = 'shop.urls'
